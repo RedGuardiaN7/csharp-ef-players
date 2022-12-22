1564ab1acc0d51082b9c13a21b34140fcc572968
RedGuardiaN7 <artiom.str@hotmail.it>
2022-12-22 14:12:38 +0100

    Add project files.

---
+++ CS Entity Framework Players/Player.cs
@@ -0,0 +1,16 @@
+﻿//Consegna: Milestone 0:
+//Modelliamo un’entità Player che rappresenta un giocatore di basket
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Entity_Framework_Players
+{
+    public class Player
+    {
+
+    }
+}
